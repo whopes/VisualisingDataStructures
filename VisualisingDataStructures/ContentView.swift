adc5a3dddde899c029b670a4f55858d38ea02c8c
whopes <hopes.w@etoncollege.org.uk>
2023-03-15 21:56:08 +0000

    Initial Commit

---
+++ VisualisingDataStructures/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VisualisingDataStructures
+//
+//  Created by Hopes, William (AMM) on 15/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VisualisingDataStructures/VisualisingDataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataStructuresApp.swift
+//  VisualisingDataStructures
+//
+//  Created by Hopes, William (AMM) on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
